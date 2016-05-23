71c1b0f6480d0a03e23aa9be85ea1d18cd43af1a
code_gs <collinharris509@icloud.com>
2016-05-23 11:06:22 -0500

    Update speed.lua

--- lua/gs/gcalculus/server/speed.lua
+++ lua/gs/gcalculus/server/speed.lua
@@ -1,35 +1,41 @@
 local _R = debug.getregistry()
 util.AddNetworkString( "GS-GCompute-Speed" )
 
-local function AddPos( ent, interval, iterations )
+local function AddPos( ent, flInterval, iMaxIterations, PosTable, VelTable )
 	if ( not ent:IsValid() ) then
 		-- Entity went invalid before we could finish, abandon it
 		return
 	end
 	
 	-- Add position and engine velocity
-	ent.gs_PosTable[ ent.gs_iSpeedIterations ] = { Pos = ent:GetPos(), Vel = ent:GetVelocity() }
+	local iTblLength = #PosTable + 1
+	PosTable[ iTblLength ] = ent:GetPos()
+	VelTable[ iTblLength ] = ent:GetVelocity()
 	
 	-- We're done; send it to the client
-	if ( ent.gs_iSpeedIterations == iterations ) then
-		FinishCalc( ent )
+	if ( iTblLength == iMaxIterations ) then
+		-- Send our data to the client to draw the graphs
+		net.Start( "GS-GCompute-Speed" )
+			net.WriteFloat( flInterval ) -- For the horizontal axis
+			net.WriteUInt( iMaxIterations, 8 ) -- How many points do we have? (Max 256)
+			
+			for i = 1, iMaxIterations do
+				net.WriteDoubleVector( PosTable[i] )
+				net.WriteDoubleVector( VelTable[i] )
+			end
+		net.Send( player.GetGraphViewers() )
 	else
-		ent.gs_iSpeedIterations = ent.gs_iSpeedIterations + 1
-		
 		-- Wait for the next point
 		timer.Simple( interval, function()
-			AddPos( ent, interval, iterations )
+			if ( ent:IsValid() ) then
+				AddPos( ent, flInterval, iMaxIterations, PosTable, VelTable )
+			end
 		end )
 	end
 end
 
 function _R.Entity:SetupPositionTracker( interval, iterations )
-	interval = interval or 1 -- Default 1 second
-	
-	self.gs_PosTable = { interval = interval }
-	self.gs_iSpeedIterations = 1
-	
-	AddPos( self, interval, iterations or 10 ) -- Default 10 points
+	AddPos( self, interval or 1, iterations or 10, {}, {} )
 end
 
 -- More precise than WriteVector, which uses floats
@@ -42,17 +48,4 @@ end
 -- Everyone gets the data packs by default
 function player.GetGraphViewers()
 	return player.GetAll()
-end
-
-local function FinishCalc( ent )
-	-- Send our data to the client to draw the graphs
-	net.Start( "GS-GCompute-Speed" )
-		net.WriteFloat( ent.gs_PosTable.interval ) -- For the horizontal axis
-		net.WriteUInt( ent.gs_iSpeedIterations, 8 ) -- How many points do we have?
-		
-		for i = 1, ent.gs_iSpeedIterations do
-			net.WriteDoubleVector( ent.gs_PosTable[i].Pos )
-			net.WriteDoubleVector( ent.gs_PosTable[i].Vel )
-		end
-	net.Send( player.GetGraphViewers() )
 end
